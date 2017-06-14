f15a7de3f9f6f315e620bc6246eb3aacccf682ee
lemome88 <lmontalvillo001@ehu.es>
2017-06-14 15:17:27 +0200

    second commit

--- sensors.js
+++ sensors.js
@@ -4,6 +4,11 @@ function applyPressure() {
 	var pointer = document.getElementById("p_point");
 	
 	applyTachoValue(minPres, maxPres, measureText, pointer);
+	//line 1 added
+	//line 2 added
+	//line 3 added
+	//line 4 added
+	//line 4 added
 	return false;
 }
 // PV:ENDCOND
@@ -17,5 +22,7 @@ function applyWindSpeed() {
 	
 	applyTachoValue(minWind, maxWind, measureText, pointer);
 	setWarnings();
+		//line 1 added
+	//line 2 added
 	return false;}
 // PV:ENDCOND
